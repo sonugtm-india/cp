35c811a0ae26d07d93039cd39b8109e586c0f189
sonugtm-india <96379972+sonugtm-india@users.noreply.github.com>
2023-05-07 00:14:31 +0530

    Update cp_simple_snippet.cpp

--- cp_simple_snippet.cpp
+++ cp_simple_snippet.cpp
@@ -58,7 +58,7 @@ int32_t main()
 {
 	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-f#ifndef ONLINE_JUDGE
+#ifndef ONLINE_JUDGE
 	freopen("input.txt",  "r",  stdin);
 	freopen("output.txt", "w", stdout);
 #endi
